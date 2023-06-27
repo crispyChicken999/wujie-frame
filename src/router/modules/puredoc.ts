dad31340dce7de60b0ce3a94ac04eb362e778099
chenxiaojun <1483091515@qq.com>
2023-06-27 10:47:42 +0800

    feat: 引入官网文档链接

---
+++ src/router/modules/puredoc.ts
@@ -0,0 +1,24 @@
+// import hot from "@/assets/svg/hot.svg?component";
+const IFrame = () => import("@/layout/frameView.vue");
+
+export default {
+  path: "/pure-admin-doc",
+  redirect: "/pure-admin-doc/index",
+  meta: {
+    icon: "iconamoon:file-document-duotone",
+    title: "pure-admin-doc",
+    rank: 2
+  },
+  children: [
+    {
+      path: "/pure-admin-doc/index",
+      name: "FrameDoc",
+      component: IFrame,
+      meta: {
+        title: "pure-admin-doc",
+        keepAlive: true,
+        frameSrc: "https://yiming_chang.gitee.io/pure-admin-doc/"
+      }
+    }
+  ]
+} as RouteConfigsTable;
